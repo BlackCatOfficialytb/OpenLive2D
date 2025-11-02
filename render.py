d7a0496d2277892a51383349253ae1a8c17f6da8
BlackCatOfficial 💀 (Python, C++, Lua) <quanvm0501alt1@tutamail.com>
2025-11-02 10:42:21 +0700

    Remove comment on Panda3D and Ursina Engine
    Remove comment about Panda3D being unusable on Linux.

--- render.py
+++ render.py
@@ -7,7 +7,6 @@ import argparse
 import subprocess
 import datetime
 test = False
-# Somehow Panda3D is unusable in the Linux so just replacing it with panda3d fork like Ursina Engine
 
 #     if datetime.datetime.now() == datetime.datetime(month=8, day=31):
 #         print(":) happy birthday miku")
